07a8499bb189ffd5b9712109b113db78f65a01da
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-02-21 23:37:01 +0100

    wip(header): created a first version of the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,53 +42,76 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={texGyreTermes.className}>
-        <header>
-          <Image
-            src="/lassist47/images/logos/full_logo_gold.png"
-            alt="Logo"
-            width={100}
-            height={100}
-          />
-          <nav aria-label="Main navigation">
-            <ul>
-              <li>
-                <Link href="/accueil">Accueil</Link>
-              </li>
-              <li>
-                <Link href="/prestations">Prestations</Link>
-              </li>
-              <li>
-                <Link href="/tarifs">Tarifs</Link>
-              </li>
-              <li>
-                <Link href="/contact">Contactez-moi</Link>
-              </li>
-            </ul>
-          </nav>
-          <section arial-label="Social media">
-            <ul>
-              <li>
-                <Link href="/mentions-legales">
-                  <Image
-                    src="/lassist47/images/logos/logo_linkedin.png"
-                    alt="LinkedIn"
-                    width={50}
-                    height={50}
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link href="/politique-de-confidentialite">
-                  <Image
-                    src="/lassist47/images/logos/logo_facebook.png"
-                    alt="Facebook"
-                    width={50}
-                    height={50}
-                  />
-                </Link>
-              </li>
-            </ul>
-          </section>
+        <header className="bg-nblack border-b-4 border-gold">
+          <div className="container mx-auto flex justify-between items-end">
+            <Image
+              src="/lassist47/images/logos/full_logo_gold.png"
+              alt="Logo"
+              className="m-4"
+              width={150}
+              height={200}
+            />
+            <nav aria-label="Main navigation">
+              <ul className="flex space-x-4">
+                <li className="bg-gold text-nblack p-2 me-2 min-w-40 text-center">
+                  <Link
+                    className="block w-full h-full hover:underline"
+                    href="/accueil"
+                  >
+                    Accueil
+                  </Link>
+                </li>
+                <li className="bg-gold text-nblack p-2 me-2 min-w-40 text-center">
+                  <Link
+                    className="block w-full h-full hover:underline"
+                    href="/prestations"
+                  >
+                    Prestations
+                  </Link>
+                </li>
+                <li className="bg-gold text-nblack p-2 me-2 min-w-40 text-center">
+                  <Link
+                    className="block w-full h-full hover:underline"
+                    href="/tarifs"
+                  >
+                    Tarifs
+                  </Link>
+                </li>
+                <li className="bg-gold text-nblack p-2 me-2 min-w-40 text-center">
+                  <Link
+                    className="block w-full h-full hover:underline"
+                    href="/contact"
+                  >
+                    Contactez-moi
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <section arial-label="Social media" aria-label="Social media links">
+              <ul className="flex space-x-4">
+                <li className="bg-gold text-nblack p-1 me-2">
+                  <Link href="/mentions-legales">
+                    <Image
+                      src="/lassist47/images/logos/logo_linkedin.png"
+                      alt="LinkedIn"
+                      width={33}
+                      height={33}
+                    />
+                  </Link>
+                </li>
+                <li className="bg-gold text-nblack p-1 me-2">
+                  <Link href="/politique-de-confidentialite">
+                    <Image
+                      src="/lassist47/images/logos/logo_facebook.png"
+                      alt="Facebook"
+                      width={33}
+                      height={33}
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </section>
+          </div>
         </header>
         <main>{children}</main>
         <footer>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        violine: "var(--violine)",
+        gold: "var(--gold)",
+        nblack: "var(--nblack)",
       },
     },
   },
