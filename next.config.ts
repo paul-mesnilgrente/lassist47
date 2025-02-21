41af720475d5161cb7f16f3a6c9d65affbcfc65d
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-02-21 13:29:35 +0100

    conf: added the assetPrefix to have working images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/lassist47',
+  // I can improve this part
+  // Check out https://nextjs.org/docs/app/building-your-application/deploying/static-exports#image-optimization
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
