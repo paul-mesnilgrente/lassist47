c19e5d6137c64a87188fd3f48338d34bb66c6d28
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-02-24 00:23:43 +0100

    feat: added photo on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,54 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl pb-3 font-bold">Présentation</h1>
-      <p className="pb-2">
-        Bonjour, je suis Ludivine Pelosato, assistante administrative
-        indépendante. Passionnée par l&apos;organisation et la gestion,
-        l&apos;administration est ma véritable vocation. J&apos;ai développé mes
-        compétences au sein d&apos;une coopérative agricole et j&apos;ai
-        également acquis de l&apos;expérience en tant que secrétaire de CSE.
-        Aujourd&apos;hui, je démarre une activité en complément de revenu dans
-        le but, à terme, de me consacrer pleinement à ma micro-entreprise. Mon
-        objectif est d&apos;accompagner les professionnels et particuliers dans
-        la gestion de leurs tâches administratives, en offrant des solutions sur
-        mesure pour simplifier leur quotidien.
-      </p>
-      <p className="pb-2">
-        Je suis convaincue que l&apos;administration, loin d&apos;être une
-        contrainte, peut devenir un atout majeur pour structurer et développer
-        son activité. Ensemble, transformons votre administratif en un levier de
-        sérénité et de réussite !
-      </p>
-      <p className="pb-2">Mes valeurs :</p>
-      <ul>
-        <li>
-          <strong>Professionnalisme</strong> : Chaque dossier est traité avec
-          rigueur et confidentialité.
-        </li>
-        <li>
-          <strong>Proximité</strong> : Un accompagnement humain et personnalisé.
-        </li>
-        <li>
-          <strong>Efficacité</strong> : Des solutions adaptées à vos besoins
-          pour gagner du temps.
-        </li>
-      </ul>
+      <div className="flex justify-between space-x-6">
+        <Image
+          src="/lassist47/images/portrait.webp"
+          alt="Portrait de Ludivine Pelosato"
+          className=""
+          width={300}
+          height={400}
+        />
+        <div>
+          <p className="pb-2">
+            Bonjour, je suis Ludivine Pelosato, assistante administrative
+            indépendante. Passionnée par l&apos;organisation et la gestion,
+            l&apos;administration est ma véritable vocation. J&apos;ai développé
+            mes compétences au sein d&apos;une coopérative agricole et j&apos;ai
+            également acquis de l&apos;expérience en tant que secrétaire de CSE.
+            Aujourd&apos;hui, je démarre une activité en complément de revenu
+            dans le but, à terme, de me consacrer pleinement à ma
+            micro-entreprise. Mon objectif est d&apos;accompagner les
+            professionnels et particuliers dans la gestion de leurs tâches
+            administratives, en offrant des solutions sur mesure pour simplifier
+            leur quotidien.
+          </p>
+          <p className="pb-2">
+            Je suis convaincue que l&apos;administration, loin d&apos;être une
+            contrainte, peut devenir un atout majeur pour structurer et
+            développer son activité. Ensemble, transformons votre administratif
+            en un levier de sérénité et de réussite !
+          </p>
+          <p className="pb-2">Mes valeurs :</p>
+          <ul>
+            <li>
+              <strong>Professionnalisme</strong> : Chaque dossier est traité
+              avec rigueur et confidentialité.
+            </li>
+            <li>
+              <strong>Proximité</strong> : Un accompagnement humain et
+              personnalisé.
+            </li>
+            <li>
+              <strong>Efficacité</strong> : Des solutions adaptées à vos besoins
+              pour gagner du temps.
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
