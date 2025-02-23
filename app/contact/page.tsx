08b5e2f184e36410bc72afaf1d463021b29385d2
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-02-23 23:05:48 +0100

    wip(contact): created inputs with...
    good background text colors

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,68 @@
 export default function Contact() {
   return (
     <>
-      <h1 className="text-2xl pb-3">Contactez-moi</h1>
+      <h1 className="text-4xl mb-11">Contactez-moi</h1>
 
-      <p className="pb-2">
-        Vous avez une question, un projet ou une demande de devis ?
-        N&apos;hésitez pas à me contacter par mail ou par téléphone.
-      </p>
+      <form className="flex flex-col gap-11">
+        <label htmlFor="name" className="block pb-1 sr-only">
+          Nom/Prénom
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Nom / Prénom"
+          className="w-full bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
+        />
+
+        <label htmlFor="phone_number" className="block pb-1 sr-only">
+          Numéro de téléphone
+        </label>
+        <input
+          type="name"
+          id="phone_number"
+          name="phone_number"
+          placeholder="N° de téléphone"
+          className="w-full bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
+        />
+
+        <label htmlFor="email" className="block pb-1 sr-only">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Adresse Mail"
+          className="w-full bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
+        />
+
+        <label htmlFor="message" className="block pb-1 sr-only">
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Message"
+          rows={5}
+          className="w-full bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
+        ></textarea>
+
+        <div className="flex justify-between">
+          <button
+            type="reset"
+            className="bg-white text-nblack px-6 py-2 text-2xl uppercase"
+          >
+            Captcha
+          </button>
+          <button
+            type="submit"
+            className="bg-violine text-gold px-6 py-2 text-2xl uppercase"
+          >
+            Envoyer
+          </button>
+        </div>
+      </form>
     </>
   );
 }
