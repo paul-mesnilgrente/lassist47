00601e9f92fd3a8b2b66f91f4fcf43bd04471615
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-02-23 14:43:18 +0100

    wip(contact): started the contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <>
+      <h1 className="text-2xl pb-3">Contactez-moi</h1>
+
+      <p className="pb-2">
+        Vous avez une question, un projet ou une demande de devis ?
+        N&apos;hésitez pas à me contacter par mail ou par téléphone.
+      </p>
+    </>
+  );
+}
