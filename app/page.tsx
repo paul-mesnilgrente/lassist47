4fdf77d3950c241b4882751155bc8597aea7b8dc
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-02-23 14:06:55 +0100

    feat: added presentation text

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,39 @@
 export default function Home() {
-  return <h1>hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-2xl pb-3">Présentation</h1>
+      <p className="pb-2">
+        Bonjour, je suis Ludivine Pelosato, assistante administrative
+        indépendante. Passionnée par l'organisation et la gestion,
+        l'administration est ma véritable vocation. J'ai développé mes
+        compétences au sein d’une coopérative agricole et j'ai également acquis
+        de l'expérience en tant que secrétaire de CSE. Aujourd’hui, je démarre
+        une activité en complément de revenu dans le but, à terme, de me
+        consacrer pleinement à ma micro-entreprise. Mon objectif est
+        d’accompagner les professionnels et particuliers dans la gestion de
+        leurs tâches administratives, en offrant des solutions sur mesure pour
+        simplifier leur quotidien.
+      </p>
+      <p className="pb-2">
+        Je suis convaincue que l’administration, loin d’être une contrainte,
+        peut devenir un atout majeur pour structurer et développer son activité.
+        Ensemble, transformons votre administratif en un levier de sérénité et
+        de réussite !
+      </p>
+      <p className="pb-2">Mes valeurs :</p>
+      <ul>
+        <li>
+          <strong>Professionnalisme</strong> : Chaque dossier est traité avec
+          rigueur et confidentialité.
+        </li>
+        <li>
+          <strong>Proximité</strong> : Un accompagnement humain et personnalisé.
+        </li>
+        <li>
+          <strong>Efficacité</strong> : Des solutions adaptées à vos besoins
+          pour gagner du temps.
+        </li>
+      </ul>
+    </>
+  );
 }
