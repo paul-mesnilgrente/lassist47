624380e2bef6d697d6c4170d1357f8638a783136
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-02-28 21:46:22 +0100

    feat: finished the desktop design on contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,42 +1,52 @@
+import Image from "next/image";
+
 export default function Contact() {
   return (
     <>
-      <h1 className="text-4xl mb-11">Contactez-moi</h1>
-
       <form className="flex flex-col gap-11">
-        <label htmlFor="name" className="block pb-1 sr-only">
-          Nom/Prénom
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Nom / Prénom"
-          className="w-full bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
-        />
+        <div className="flex items-end justify-between">
+          <div className="flex flex-grow mr-6 flex-col gap-11">
+            <label htmlFor="name" className="block pb-1 sr-only">
+              Nom/Prénom
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Nom / Prénom"
+              className="w-1/2 bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
+            />
 
-        <label htmlFor="phone_number" className="block pb-1 sr-only">
-          Numéro de téléphone
-        </label>
-        <input
-          type="name"
-          id="phone_number"
-          name="phone_number"
-          placeholder="N° de téléphone"
-          className="w-full bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
-        />
+            <label htmlFor="phone_number" className="block pb-1 sr-only">
+              Numéro de téléphone
+            </label>
+            <input
+              type="tel"
+              id="phone_number"
+              name="phone_number"
+              placeholder="N° de téléphone"
+              className="w-1/3 bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
+            />
 
-        <label htmlFor="email" className="block pb-1 sr-only">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Adresse Mail"
-          className="w-full bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
-        />
-
+            <label htmlFor="email" className="block pb-1 sr-only">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Adresse Mail"
+              className="w-4/5 bg-violine px-4 py-1 text-2xl text-gold placeholder:text-gold"
+            />
+          </div>
+          <Image
+            src="/lassist47/images/logos/logo_contact.png"
+            className="mb-11"
+            width={200}
+            height={200}
+            alt=""
+          />
+        </div>
         <label htmlFor="message" className="block pb-1 sr-only">
           Message
         </label>
