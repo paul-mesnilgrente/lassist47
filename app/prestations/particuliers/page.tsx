702c8cca08e6fb3d44093e608f9252e9c92fd249
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2025-02-28 23:59:23 +0100

    feat: added interactive tabs on prestations

--- app/prestations/particuliers/page.tsx
+++ app/prestations/particuliers/page.tsx
@@ -1,29 +1,53 @@
-import ContentContainer from "@/components/ContentContainer";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 export default function Particuliers() {
   return (
-    <ContentContainer>
-      <h1 className="text-2xl">Assistance administrative</h1>
-      <ul className="list-disc mb-4">
-        <li>Rédaction, relecture et mise en page de document et courrier</li>
-        <li>Formalités administratives (CNI, passeport, CAF, CPAM, MSA,…)</li>
-        <li>Déclarations diverses (France Travail, PAJEmploi, impôts,…)</li>
-        <li>Gestion de budget</li>
-      </ul>
-
-      <h1 className="text-2xl">Initiations informatiques</h1>
-      <ul className="list-disc mb-4">
-        <li>Initiation aux outils microsoft (word, excel, powerpoint)</li>
-        <li>
-          Initiation à l&apos;outil informatique (fonctions de base d&apos;un
-          ordinateur)
-        </li>
-        <li>Initiation aux outils en ligne (bases d&apos;internet, mail)</li>
-        <li>
-          Initiation aux réseaux sociaux (Facebook, Linkedin, Instagram, Tik
-          Tok)
-        </li>
-      </ul>
-    </ContentContainer>
+    <div className="w-3/4 mx-auto">
+      <TabGroup>
+        <TabList
+          aria-label="Tab des prestations"
+          className="flex space-x-4 uppercase"
+        >
+          <Tab className="p-2 min-w-40 text-center bg-gold text-nblack hover:underline">
+            Assistance administrative
+          </Tab>
+          <Tab className="p-2 min-w-40 text-center bg-gold text-nblack hover:underline">
+            Initiations informatiques
+          </Tab>
+        </TabList>
+        <TabPanels className="p-11 bg-nblack border-4 border-gold">
+          <TabPanel>
+            <ul className="list-disc mb-4">
+              <li>
+                Rédaction, relecture et mise en page de document et courrier
+              </li>
+              <li>
+                Formalités administratives (CNI, passeport, CAF, CPAM, MSA,…)
+              </li>
+              <li>
+                Déclarations diverses (France Travail, PAJEmploi, impôts,…)
+              </li>
+              <li>Gestion de budget</li>
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="list-disc mb-4">
+              <li>Initiation aux outils microsoft (word, excel, powerpoint)</li>
+              <li>
+                Initiation à l&apos;outil informatique (fonctions de base
+                d&apos;un ordinateur)
+              </li>
+              <li>
+                Initiation aux outils en ligne (bases d&apos;internet, mail)
+              </li>
+              <li>
+                Initiation aux réseaux sociaux (Facebook, Linkedin, Instagram,
+                Tik Tok)
+              </li>
+            </ul>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </div>
   );
 }
--- app/prestations/professionnels/page.tsx
+++ app/prestations/professionnels/page.tsx
@@ -1,70 +1,98 @@
-import ContentContainer from "@/components/ContentContainer";
+"use client";
+
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 export default function Professionnels() {
   return (
-    <ContentContainer>
-      <h1 className="text-2xl">Assistance administrative</h1>
-      <ul className="list-disc mb-4">
-        <li>Gestion de planning et agenda</li>
-        <li>Gestion courrier / mails (traitement, rédaction…)</li>
-        <li>
-          Création et mise à jour de document type, tableaux de bord, suivis…
-        </li>
-        <li>
-          Rédaction, relecture, mise en page, de vos courriers, rapports,
-          comptes rendus, procès-verbaux…
-        </li>
-        <li>Classement, tri et archivage</li>
-        <li>Préparation d&apos;un déplacement professionnel</li>
-        <li>Gestion statutaire (assemblée générale, formalité INPI,…)</li>
-        <li>Prospection de partenariats CSE</li>
-        <li>Recherche documentaire et règlementaire</li>
-      </ul>
+    <div className="w-3/4 mx-auto">
+      <TabGroup>
+        <TabList
+          aria-label="Tab des prestations"
+          className="flex space-x-4 uppercase"
+        >
+          <Tab className="p-2 min-w-40 text-center bg-gold text-nblack hover:underline">
+            Assistance administrative
+          </Tab>
+          <Tab className="p-2 min-w-40 text-center bg-gold text-nblack hover:underline">
+            Assistance commerciale
+          </Tab>
+          <Tab className="p-2 min-w-40 text-center bg-gold text-nblack hover:underline">
+            Assistance pré-comptable
+          </Tab>
+          <Tab className="p-2 min-w-40 text-center bg-gold text-nblack hover:underline">
+            Assistance ressources humaines
+          </Tab>
+        </TabList>
 
-      <h1 className="text-2xl">Assistance commerciale</h1>
-      <ul className="list-disc mb-4">
-        <li>Elaboration de devis</li>
-        <li>Suivi et relance</li>
-        <li>Traitement des réclamations</li>
-        <li>Création et suivi d&apos;une base de données clients</li>
-        <li>
-          Organisation d&apos;évènement (séminaire, assemblée générale, journée
-          portes ouvertes,…)
-        </li>
-      </ul>
-
-      <h1 className="text-2xl">Assistance pré-comptable</h1>
-      <ul className="list-disc mb-4">
-        <li>Préparation des éléments à remettre à votre cabinet comptable</li>
-        <li>Suivi des encaissements et relance des impayés</li>
-        <li>Contrôle et suivi des factures fournisseurs et clients</li>
-        <li>
-          Suivi de la trésorerie
-          <br />
-          Je n&apos;ai pas le droit d&apos;exercer la comptabilité, ceci est un
-          domaine réservé aux experts comptables selon l&apos;
-          <a
-            href="https://www.legifrance.gouv.fr/loda/article_lc/LEGIARTI000033678961"
-            target="_blank"
-            rel="noreferrer noopener"
-            className="underline hover:text-gold"
-          >
-            ordonnance du 19 septembre 1945
-          </a>
-          . Je n&apos;interviens donc jamais sur logiciel comptable, tant dans
-          mon intérêt que dans le vôtre.
-        </li>
-      </ul>
-
-      <h1 className="text-2xl">Assistance ressources humaines</h1>
-      <ul className="list-disc mb-4">
-        <li>Création et mise en ligne d&apos;offre d&apos;emploi</li>
-        <li>Pré-sélection de CV</li>
-        <li>Entretien préalable</li>
-        <li>Mise à jour du registre du personnel</li>
-        <li>Suivi des visites médicales</li>
-        <li>Préparation des documents d&apos;embauche</li>
-      </ul>
-    </ContentContainer>
+        <TabPanels className="p-11 bg-nblack border-4 border-gold">
+          <TabPanel>
+            <ul className="list-disc">
+              <li>Gestion de planning et agenda</li>
+              <li>Gestion courrier / mails (traitement, rédaction…)</li>
+              <li>
+                Création et mise à jour de document type, tableaux de bord,
+                suivis…
+              </li>
+              <li>
+                Rédaction, relecture, mise en page, de vos courriers, rapports,
+                comptes rendus, procès-verbaux…
+              </li>
+              <li>Classement, tri et archivage</li>
+              <li>Préparation d&apos;un déplacement professionnel</li>
+              <li>Gestion statutaire (assemblée générale, formalité INPI,…)</li>
+              <li>Prospection de partenariats CSE</li>
+              <li>Recherche documentaire et règlementaire</li>
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="list-disc">
+              <li>Élaboration de devis</li>
+              <li>Suivi et relance</li>
+              <li>Traitement des réclamations</li>
+              <li>Création et suivi d&apos;une base de données clients</li>
+              <li>
+                Organisation d&apos;évènement (séminaire, assemblée générale,
+                journée portes ouvertes,…)
+              </li>
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="list-disc">
+              <li>
+                Préparation des éléments à remettre à votre cabinet comptable
+              </li>
+              <li>Suivi des encaissements et relance des impayés</li>
+              <li>Contrôle et suivi des factures fournisseurs et clients</li>
+              <li>
+                Suivi de la trésorerie
+                <br />
+                Je n&apos;ai pas le droit d&apos;exercer la comptabilité, ceci
+                est un domaine réservé aux experts comptables selon l&apos;
+                <a
+                  href="https://www.legifrance.gouv.fr/loda/article_lc/LEGIARTI000033678961"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="underline hover:text-gold"
+                >
+                  ordonnance du 19 septembre 1945
+                </a>
+                . Je n&apos;interviens donc jamais sur logiciel comptable, tant
+                dans mon intérêt que dans le vôtre.
+              </li>
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="list-disc">
+              <li>Création et mise en ligne d&apos;offre d&apos;emploi</li>
+              <li>Pré-sélection de CV</li>
+              <li>Entretien préalable</li>
+              <li>Mise à jour du registre du personnel</li>
+              <li>Suivi des visites médicales</li>
+              <li>Préparation des documents d&apos;embauche</li>
+            </ul>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </div>
   );
 }
